03a1c81fcdfdcb5c4df517a440534e15a208ca67
valdemar <vladyslav.sydiuk@gmail.com>
2023-10-19 16:21:53 +0300

    Update app with CORS support

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     # local Apps,
     'accounts',
     'articles',
-    'comments'
+    'comments',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -59,9 +60,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
